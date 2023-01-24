ca5cdd0fa9012ef6bcfcd9ac9e270e8e73da4622
mrx7355608 <mrx7355608@gmail.com>
2023-01-24 11:03:36 +0500

    created a generic type for repositories

---
+++ src/core/interfaces/generic.repo.ts
@@ -0,0 +1,8 @@
+export abstract class GenericRepository<T> {
+    abstract findAll(): Promise<T[]>;
+    abstract findById(id: string): Promise<T>;
+    abstract findOne(field: string): Promise<T>;
+    abstract insert(data: T): Promise<T>;
+    abstract update(id: string, changes: T): Promise<T>;
+    abstract delete(id: string): Promise<Boolean>;
+}
