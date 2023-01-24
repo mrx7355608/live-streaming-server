fcd2ce5b5ec42ca35177902810cc5ed2100d6f48
mrx7355608 <mrx7355608@gmail.com>
2023-01-24 11:24:50 +0500

    created user services

---
+++ src/core/interactors/index.ts
@@ -0,0 +1,14 @@
+import listAllUsersFactory from './listAllUsers';
+import listOneUserFactory from './listOneUser';
+import addUserFactory from './addUser';
+import { UserDB } from '@/data/user.data';
+import { HashService } from 'services/hash.services';
+
+const userDB = new UserDB();
+const hashService = new HashService();
+
+export const userServices = {
+    listAllUsers: listAllUsersFactory(userDB),
+    listOneUser: listOneUserFactory(userDB),
+    addUser: addUserFactory(userDB, hashService),
+};
