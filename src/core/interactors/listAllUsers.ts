4437ba805c553e700371ee66c50875e3966c41f9
mrx7355608 <mrx7355608@gmail.com>
2023-01-25 09:24:51 +0500

    gave function a return type

--- src/core/interactors/listAllUsers.ts
+++ src/core/interactors/listAllUsers.ts
@@ -2,7 +2,7 @@ import { GenericRepository } from '../interfaces/generic.repo';
 import { IUser } from '../interfaces/user.interface';
 
 export default function listAllUsersFactory(userDB: GenericRepository<IUser>) {
-    return async function () {
+    return async function (): Promise<IUser[]> {
         // TODO: add pagination and query search
         return await userDB.findAll();
     };
